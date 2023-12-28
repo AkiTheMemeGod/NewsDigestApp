b64a618d1ee6b53d155b288702224f91c9ef7807
AkiTheMemeGod <k.akashkumar@gmail.com>
2023-12-28 18:40:50 +0530

    made some cosmetic changes to the containers and the way they behave

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 import streamlit as st
 import requests as rs
 import os
-st.set_page_config(layout="wide",page_title="Daily News")
+st.set_page_config(layout="wide", page_title="Daily News")
 api_key = os.getenv('NEWSAPI')
 sortby_options = ["relevancy", "popularity", "publishedAt"]
-st.title("Your Daily News")
+c1, c2, c3 = st.columns([4, 3, 3])
+with c2:
+    st.title("YOUR DAILY NEWS")
+    st.markdown("###")
+    st.markdown("###")
 
 
 def fetch():
     url = f"https://newsapi.org/v2/everything?q={query}&sortBy={sortBy}&apiKey={api_key}&language={language}"
     req = rs.get(url)
-    content = req.json()
-    return content
+    cont = req.json()
+    return cont
 
-n1, n2 = st.columns([2,1])
+
+n1, n2 = st.columns([2, 1])
 with n1:
-    query = st.text_input(label="Read about ? ", placeholder="ex: 'Tesla'",on_change=fetch)
+    query = st.text_input(label="Read about ? ", placeholder="ex: 'Tesla'", on_change=fetch)
 with n2:
     st.markdown("###")
     with st.expander("🛞 Filters"):
-        limit = st.slider(label="Feed limit",min_value=0, max_value=100,step=10,on_change=fetch)
-        sortBy = st.radio(label="Filter", options=sortby_options,on_change=fetch)
-        language = st.radio(label="language", options=["en", "es", "fr", "ru"],on_change=fetch)
-
+        limit = st.slider(label="Feed limit", min_value=0, max_value=100, step=10, on_change=fetch)
+        sortBy = st.radio(label="Filter", options=sortby_options, on_change=fetch)
+        language = st.radio(label="language", options=["en", "es", "fr", "ru"], on_change=fetch)
+    st.markdown("###")
+    st.markdown("###")
 n1, n2, n3 = st.columns(3)
 content = fetch()
 try:
@@ -30,26 +36,33 @@ try:
         for article in content["articles"][0:round((int(limit))/3)]:
             if article is not None:
                 with st.container(border=1):
-                    st.subheader(article["title"])
+                    st.subheader(f':blue[{article["title"]}]')
+                    st.markdown("---")
                     st.write(article["description"])
-                    st.success(article["url"])
+                    st.markdown("###")
+                    st.info(f':green[{article["url"]}]')
+            st.markdown("###")
 
     with n2:
-        for article in content["articles"][round((int(limit))/3):round(2*(int(limit))/3)]:
+        for article in content["articles"][round((int(limit))/3)-1:round(2*(int(limit))/3)]:
             if article is not None:
                 with st.container(border=1):
-                    st.subheader(article["title"])
+                    st.subheader(f':blue[{article["title"]}]')
+                    st.markdown("---")
                     st.write(article["description"])
-                    st.success(article["url"])
-
+                    st.markdown("###")
+                    st.info(f':green[{article["url"]}]')
+            st.markdown("###")
     with n3:
         for article in content["articles"][round(2*(int(limit))/3):int(limit)]:
             if article is not None:
                 with st.container(border=1):
-                    st.subheader(article["title"])
+                    st.subheader(f':blue[{article["title"]}]')
+                    st.markdown("---")
                     st.write(article["description"])
-                    st.success(article["url"])
-
+                    st.markdown("###")
+                    st.info(f':green[{article["url"]}]')
+            st.markdown("###")
 except KeyError:
     with n2:
         st.markdown("###")
@@ -59,4 +72,4 @@ except KeyError:
         st.markdown("###")
         st.markdown("###")
         st.markdown("###")
-        st.error("Try Searching for something in the searchbar", icon="⚠️")
+        st.info(" Try Searching for something in the searchbar", icon="📰")
